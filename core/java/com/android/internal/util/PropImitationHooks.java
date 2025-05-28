8cd3d1167e8ba1af0e8f024fe59aeb0fc72f3611
Abhay Singh Gill <abhaygill017@gmail.com>
2025-05-29 03:59:29 +0530

    base: Add key generation for keybox spoofing [1/2]
    The code is used from :-
    https://github.com/5ec1cff/TrickyStore
    
    This allows us to enjoy spoofing keyboxes the right way even with broken TEE.
    Allow users to provide keybox XML as well.
    
    Change-Id: I0da036c907f8cd1f6d2e3480cf3d5f688ac17dc0
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -31,6 +31,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import com.android.internal.R;
+import com.android.internal.util.custom.KeyProviderManager;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
