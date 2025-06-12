a4c78440fe114fb9223048680b1cee850ae5fb90
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-06-12 08:45:30 +0530

    base: Move PI/KIHooks to aospa package
    Change-Id: I1c6732520565a9f677956e0d2e6f9d1edd2cf989

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -133,7 +133,7 @@ import com.android.internal.annotations.GuardedBy;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.os.SomeArgs;
 import com.android.internal.pm.RoSystemFeatures;
-import com.android.internal.util.PropImitationHooks;
+import com.android.internal.util.aospa.PropImitationHooks;
 import com.android.internal.util.UserIcons;
 
 import dalvik.system.VMRuntime;
