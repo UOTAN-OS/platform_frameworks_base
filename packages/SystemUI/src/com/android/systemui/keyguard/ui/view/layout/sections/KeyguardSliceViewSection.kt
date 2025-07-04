81d71cd4e47e781eb9bc07856e171f9a489d28d2
Fred Underwood <underwoodfred@proton.me>
2025-07-05 07:30:30 +1000

    SystemUI: fix Keyguard slice
    Upstream refactor in commit 1f73b12d5dd4db6ebcad5d444c79ac18cc59e3c3
    removed too much and as a result the legacy Keyguard slice code was
    broken. This adds back the needed elements.
    
    Change-Id: I0ba66d98bb8b76c48a204ff4cb37bcf41154a4ae

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
@@ -28,9 +28,11 @@ import com.android.keyguard.KeyguardSliceViewController
 import com.android.systemui.dagger.qualifiers.Background
 import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.dump.DumpManager
+import com.android.systemui.keyguard.domain.interactor.KeyguardInteractor
 import com.android.systemui.keyguard.shared.model.KeyguardSection
 import com.android.systemui.plugins.ActivityStarter
 import com.android.systemui.plugins.clocks.ClockViewIds
+import com.android.systemui.power.domain.interactor.PowerInteractor
 import com.android.systemui.res.R
 import com.android.systemui.settings.DisplayTracker
 import com.android.systemui.statusbar.lockscreen.LockscreenSmartspaceController
@@ -49,6 +51,8 @@ constructor(
     val configurationController: ConfigurationController,
     val dumpManager: DumpManager,
     val displayTracker: DisplayTracker,
+    val keyguardInteractor: KeyguardInteractor,
+    val powerInteractor: PowerInteractor,
 ) : KeyguardSection() {
     private lateinit var sliceView: KeyguardSliceView
 
@@ -72,6 +76,8 @@ constructor(
                 configurationController,
                 dumpManager,
                 displayTracker,
+                keyguardInteractor,
+                powerInteractor,
             )
         controller.init()
     }
