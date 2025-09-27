32787f691b3daf1c378d9d73e1a15203daea2f79
Abhay Singh Gill <abhaygill017@gmail.com>
2025-09-28 02:58:23 +0530

    PropImitationHooks: Add option for GMS to get cert chain
    [hridaya: Update commit message]
    
    Change-Id: Ie513c02489341d19da33f44c1fa8ec266913a501
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -19,6 +19,7 @@
 package com.android.internal.util;
 
 import android.app.ActivityTaskManager;
+import android.app.ActivityThread;
 import android.app.Application;
 import android.app.TaskStackListener;
 import android.content.ComponentName;
@@ -323,9 +324,15 @@ public class PropImitationHooks {
             return;
         }
 
-        // If a keybox is found, don't block key attestation
-        if (KeyProviderManager.isKeyboxAvailable()) {
-            dlog("Key attestation blocking is disabled because a keybox is defined to spoof");
+        Context context = ActivityThread.currentApplication();
+        if (context == null) {
+            Log.e(TAG, "Context is null in onEngineGetCertificateChain");
+            return;
+        }
+    
+        if ((Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.GMS_CERT_CHAIN, 0) == 1)
+                && KeyProviderManager.isKeyboxAvailable()) {
+            dlog("Allowing gms / finsky to get cert chain");
             return;
         }
 
