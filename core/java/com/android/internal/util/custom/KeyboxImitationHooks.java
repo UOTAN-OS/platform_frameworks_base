crdroidandroid/android_frameworks_base

KeyboxImitationHooks: Only generate certificates for play integrity calls
Let's just face it, keybox spoofing is broken in some apps like payzapp and IIFL market
so this is fine for now until we fix our keybox imitation impl.

Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/com/android/internal/util/crdroid/KeyboxImitationHooks.java
@@ -5,11 +5,15 @@
  */
 package com.android.internal.util.crdroid;
 
+import android.app.ActivityThread;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.security.keymint.Algorithm;
 import android.hardware.security.keymint.KeyParameter;
 import android.hardware.security.keymint.KeyParameterValue;
 import android.hardware.security.keymint.Tag;
 import android.os.Binder;
+import android.security.KeyChain;
 import android.system.keystore2.Authorization;
 import android.system.keystore2.IKeystoreSecurityLevel;
 import android.system.keystore2.KeyDescriptor;
@@ -17,13 +21,37 @@ import android.system.keystore2.KeyEntryResponse;
 import android.system.keystore2.KeyMetadata;
 import android.util.Log;
 
-import com.android.internal.util.crdroid.KeyboxChainGenerator.KeyGenParameters;
+import com.android.internal.org.bouncycastle.asn1.ASN1Boolean;
+import com.android.internal.org.bouncycastle.asn1.ASN1Encodable;
+import com.android.internal.org.bouncycastle.asn1.ASN1EncodableVector;
+import com.android.internal.org.bouncycastle.asn1.ASN1Enumerated;
+import com.android.internal.org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import com.android.internal.org.bouncycastle.asn1.ASN1OctetString;
+import com.android.internal.org.bouncycastle.asn1.ASN1Sequence;
+import com.android.internal.org.bouncycastle.asn1.ASN1TaggedObject;
+import com.android.internal.org.bouncycastle.asn1.DEROctetString;
+import com.android.internal.org.bouncycastle.asn1.DERSequence;
+import com.android.internal.org.bouncycastle.asn1.DERTaggedObject;
+import com.android.internal.org.bouncycastle.asn1.x509.Extension;
+import com.android.internal.org.bouncycastle.cert.X509CertificateHolder;
+import com.android.internal.org.bouncycastle.cert.X509v3CertificateBuilder;
+import com.android.internal.org.bouncycastle.operator.ContentSigner;
+import com.android.internal.org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
+import com.android.internal.util.crdroid.KeyboxChainGenerator.KeyGenParameters;
+import com.android.internal.util.crdroid.KeyboxUtils;
+
+import java.security.PrivateKey;
 import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @hide
@@ -33,6 +61,14 @@ public class KeyboxImitationHooks {
     private static final String TAG = "KeyboxImitationHooks";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
     private static boolean mSuccess = false;
+
+    private static final ASN1ObjectIdentifier KEY_ATTESTATION_OID = new ASN1ObjectIdentifier(
+            "1.3.6.1.4.1.11129.2.1.17");
+
+    private static final Set<String> ALLOWED_PACKAGES = new HashSet<>(Arrays.asList(
+            "com.android.vending",
+            "com.google.android.gms"
+    ));
 
     public static KeyEntryResponse onGetKeyEntry(KeyDescriptor descriptor) {
         if (!KeyProviderManager.isKeyboxAvailable()) {
@@ -52,6 +88,115 @@ public class KeyboxImitationHooks {
         return null;
     }
 
+    public static KeyEntryResponse fallbackKeyEntry(KeyEntryResponse response) {
+        // If no keybox is found, don't continue spoofing
+        if (!KeyProviderManager.isKeyboxAvailable()) {
+            dlog("Key attestation spoofing is disabled because no keybox is defined to spoof");
+            return response;
+        }
+
+        if (response == null || response.metadata == null) return response;
+
+        try {
+            if (response.metadata.certificate == null) {
+                Log.e(TAG, "Certificate is null, skipping modification");
+                return response;
+            }
+
+            X509Certificate certificate = KeyChain.toCertificate(response.metadata.certificate);
+            if (certificate.getExtensionValue(KEY_ATTESTATION_OID.getId()) == null) {
+                Log.e(TAG, "Key attestation OID not found, skipping modification");
+                return response;
+            }
+
+            String keyAlgorithm = certificate.getPublicKey().getAlgorithm();
+            response.metadata.certificate = modifyLeafCertificate(certificate, keyAlgorithm);
+            response.metadata.certificateChain = KeyboxUtils.getCertificateChainBytes(keyAlgorithm);
+        } catch (Exception e) {
+            Log.e(TAG, "Error in onGetKeyEntry", e);
+        }
+
+        return response;
+    }
+
+    private static byte[] modifyLeafCertificate(X509Certificate leafCertificate,
+            String keyAlgorithm) throws Exception {
+        X509CertificateHolder certificateHolder = new X509CertificateHolder(
+                leafCertificate.getEncoded());
+        Extension keyAttestationExtension = certificateHolder.getExtension(KEY_ATTESTATION_OID);
+        ASN1Sequence keyAttestationSequence = ASN1Sequence.getInstance(
+                keyAttestationExtension.getExtnValue().getOctets());
+        ASN1Encodable[] keyAttestationEncodables = keyAttestationSequence.toArray();
+        ASN1Sequence teeEnforcedSequence = (ASN1Sequence) keyAttestationEncodables[7];
+        ASN1EncodableVector teeEnforcedVector = new ASN1EncodableVector();
+
+        ASN1Sequence rootOfTrustSequence = null;
+        for (ASN1Encodable teeEnforcedEncodable : teeEnforcedSequence) {
+            ASN1TaggedObject taggedObject = (ASN1TaggedObject) teeEnforcedEncodable;
+            if (taggedObject.getTagNo() == 704) {
+                rootOfTrustSequence = (ASN1Sequence) taggedObject.getObject();
+                continue;
+            }
+            teeEnforcedVector.add(teeEnforcedEncodable);
+        }
+
+        if (rootOfTrustSequence == null) throw new Exception("Root of trust not found");
+
+        PrivateKey privateKey = KeyboxUtils.getPrivateKey(keyAlgorithm);
+        X509CertificateHolder providerCertHolder = KeyboxUtils.getCertificateHolder(keyAlgorithm);
+
+        X509v3CertificateBuilder certificateBuilder = new X509v3CertificateBuilder(
+                providerCertHolder.getSubject(),
+                certificateHolder.getSerialNumber(),
+                certificateHolder.getNotBefore(),
+                certificateHolder.getNotAfter(),
+                certificateHolder.getSubject(),
+                certificateHolder.getSubjectPublicKeyInfo()
+        );
+
+        ContentSigner contentSigner = new JcaContentSignerBuilder(
+                leafCertificate.getSigAlgName()).build(privateKey);
+
+        byte[] verifiedBootKey = new byte[32];
+        ThreadLocalRandom.current().nextBytes(verifiedBootKey);
+
+        DEROctetString verifiedBootHash = (DEROctetString) rootOfTrustSequence.getObjectAt(3);
+        if (verifiedBootHash == null) {
+            byte[] randomHash = new byte[32];
+            ThreadLocalRandom.current().nextBytes(randomHash);
+            verifiedBootHash = new DEROctetString(randomHash);
+        }
+
+        ASN1Encodable[] rootOfTrustEncodables = {
+                new DEROctetString(verifiedBootKey),
+                ASN1Boolean.TRUE,
+                new ASN1Enumerated(0),
+                verifiedBootHash
+        };
+
+        ASN1Sequence newRootOfTrustSequence = new DERSequence(rootOfTrustEncodables);
+        ASN1TaggedObject rootOfTrustTaggedObject = new DERTaggedObject(704, newRootOfTrustSequence);
+        teeEnforcedVector.add(rootOfTrustTaggedObject);
+
+        ASN1Sequence newTeeEnforcedSequence = new DERSequence(teeEnforcedVector);
+        keyAttestationEncodables[7] = newTeeEnforcedSequence;
+        ASN1Sequence newKeyAttestationSequence = new DERSequence(keyAttestationEncodables);
+        ASN1OctetString newKeyAttestationOctetString = new DEROctetString(
+                newKeyAttestationSequence);
+        Extension newKeyAttestationExtension = new Extension(KEY_ATTESTATION_OID, false,
+                newKeyAttestationOctetString);
+
+        certificateBuilder.addExtension(newKeyAttestationExtension);
+
+        for (ASN1ObjectIdentifier extensionOID :
+                certificateHolder.getExtensions().getExtensionOIDs()) {
+            if (KEY_ATTESTATION_OID.getId().equals(extensionOID.getId())) continue;
+            certificateBuilder.addExtension(certificateHolder.getExtension(extensionOID));
+        }
+
+        return certificateBuilder.build(contentSigner).getEncoded();
+    }
+
     public static KeyMetadata generateKey(IKeystoreSecurityLevel level, KeyDescriptor descriptor, Collection<KeyParameter> args) {
         if (!KeyProviderManager.isKeyboxAvailable()) {
             return null;
@@ -69,6 +214,36 @@ public class KeyboxImitationHooks {
         }
 
         int uid = Binder.getCallingUid();
+
+        String[] packages = null;
+        try {
+            Context context = ActivityThread.currentApplication();
+            if (context != null) {
+                PackageManager pm = context.getPackageManager();
+                packages = pm.getPackagesForUid(uid);
+            }
+        } catch (Exception e) {
+            Log.w(TAG, "Failed to resolve packages for uid " + uid, e);
+        }
+
+        if (packages == null || packages.length == 0) {
+            Log.w(TAG, "No package found for UID " + uid + ", rejecting");
+            return null;
+        }
+
+        boolean allowed = false;
+        for (String pkg : packages) {
+            if (ALLOWED_PACKAGES.contains(pkg)) {
+                allowed = true;
+                break;
+            }
+        }
+
+        if (!allowed) {
+            Log.w(TAG, "UID " + uid + " packages " + Arrays.toString(packages) + " are not allowed");
+            return null;
+        }
+
         try {
             List<Certificate> chain = KeyboxChainGenerator.generateCertChain(uid, descriptor, params);
             if (chain == null || chain.isEmpty()) {

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -90,6 +90,7 @@ import java.util.NoSuchElementException;
 
 import javax.crypto.SecretKey;
 
+import com.android.internal.util.crdroid.KeyboxImitationHooks;
 import com.android.internal.util.crdroid.PixelPropsUtils;
 
 /**
@@ -169,7 +170,10 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
         try {
             StrictMode.noteDiskRead();
-            return mKeyStore.getKeyEntry(descriptor);
+            KeyEntryResponse response = mKeyStore.getKeyEntry(descriptor);
+            KeyEntryResponse newResponse = KeyboxImitationHooks.onGetKeyEntry(descriptor);
+            response = (newResponse != null) ? newResponse : KeyboxImitationHooks.fallbackKeyEntry(response);
+            return response;
         } catch (android.security.KeyStoreException e) {
             if (e.getErrorCode() != ResponseCode.KEY_NOT_FOUND) {
                 Log.w(TAG, "Could not get key metadata from Keystore.", e);

UOTAN-OS/platform_frameworks_base

Change-Id: Iea224ebd0dfe73e3d11413613527b278e4c641ea

core/java/com/android/internal/util/custom/KeyboxImitationHooks.java
@@ -5,11 +5,15 @@
  */
 package com.android.internal.util.custom;
 
+import android.app.ActivityThread;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.security.keymint.Algorithm;
 import android.hardware.security.keymint.KeyParameter;
 import android.hardware.security.keymint.KeyParameterValue;
 import android.hardware.security.keymint.Tag;
 import android.os.Binder;
+import android.security.KeyChain;
 import android.system.keystore2.Authorization;
 import android.system.keystore2.IKeystoreSecurityLevel;
 import android.system.keystore2.KeyDescriptor;
@@ -17,13 +21,37 @@ import android.system.keystore2.KeyEntryResponse;
 import android.system.keystore2.KeyMetadata;
 import android.util.Log;
 
-import com.android.internal.util.custom.KeyboxChainGenerator.KeyGenParameters;
+import com.android.internal.org.bouncycastle.asn1.ASN1Boolean;
+import com.android.internal.org.bouncycastle.asn1.ASN1Encodable;
+import com.android.internal.org.bouncycastle.asn1.ASN1EncodableVector;
+import com.android.internal.org.bouncycastle.asn1.ASN1Enumerated;
+import com.android.internal.org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import com.android.internal.org.bouncycastle.asn1.ASN1OctetString;
+import com.android.internal.org.bouncycastle.asn1.ASN1Sequence;
+import com.android.internal.org.bouncycastle.asn1.ASN1TaggedObject;
+import com.android.internal.org.bouncycastle.asn1.DEROctetString;
+import com.android.internal.org.bouncycastle.asn1.DERSequence;
+import com.android.internal.org.bouncycastle.asn1.DERTaggedObject;
+import com.android.internal.org.bouncycastle.asn1.x509.Extension;
+import com.android.internal.org.bouncycastle.cert.X509CertificateHolder;
+import com.android.internal.org.bouncycastle.cert.X509v3CertificateBuilder;
+import com.android.internal.org.bouncycastle.operator.ContentSigner;
+import com.android.internal.org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
+import com.android.internal.util.custom.KeyboxChainGenerator.KeyGenParameters;
+import com.android.internal.util.custom.KeyboxUtils;
+
+import java.security.PrivateKey;
 import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @hide
@@ -33,6 +61,14 @@ public class KeyboxImitationHooks {
     private static final String TAG = "KeyboxImitationHooks";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
     private static boolean mSuccess = false;
+
+    private static final ASN1ObjectIdentifier KEY_ATTESTATION_OID = new ASN1ObjectIdentifier(
+            "1.3.6.1.4.1.11129.2.1.17");
+
+    private static final Set<String> ALLOWED_PACKAGES = new HashSet<>(Arrays.asList(
+            "com.android.vending",
+            "com.google.android.gms"
+    ));
 
     public static KeyEntryResponse onGetKeyEntry(KeyDescriptor descriptor) {
         if (!KeyProviderManager.isKeyboxAvailable()) {
@@ -52,6 +88,115 @@ public class KeyboxImitationHooks {
         return null;
     }
 
+    public static KeyEntryResponse fallbackKeyEntry(KeyEntryResponse response) {
+        // If no keybox is found, don't continue spoofing
+        if (!KeyProviderManager.isKeyboxAvailable()) {
+            dlog("Key attestation spoofing is disabled because no keybox is defined to spoof");
+            return response;
+        }
+
+        if (response == null || response.metadata == null) return response;
+
+        try {
+            if (response.metadata.certificate == null) {
+                Log.e(TAG, "Certificate is null, skipping modification");
+                return response;
+            }
+
+            X509Certificate certificate = KeyChain.toCertificate(response.metadata.certificate);
+            if (certificate.getExtensionValue(KEY_ATTESTATION_OID.getId()) == null) {
+                Log.e(TAG, "Key attestation OID not found, skipping modification");
+                return response;
+            }
+
+            String keyAlgorithm = certificate.getPublicKey().getAlgorithm();
+            response.metadata.certificate = modifyLeafCertificate(certificate, keyAlgorithm);
+            response.metadata.certificateChain = KeyboxUtils.getCertificateChainBytes(keyAlgorithm);
+        } catch (Exception e) {
+            Log.e(TAG, "Error in onGetKeyEntry", e);
+        }
+
+        return response;
+    }
+
+    private static byte[] modifyLeafCertificate(X509Certificate leafCertificate,
+            String keyAlgorithm) throws Exception {
+        X509CertificateHolder certificateHolder = new X509CertificateHolder(
+                leafCertificate.getEncoded());
+        Extension keyAttestationExtension = certificateHolder.getExtension(KEY_ATTESTATION_OID);
+        ASN1Sequence keyAttestationSequence = ASN1Sequence.getInstance(
+                keyAttestationExtension.getExtnValue().getOctets());
+        ASN1Encodable[] keyAttestationEncodables = keyAttestationSequence.toArray();
+        ASN1Sequence teeEnforcedSequence = (ASN1Sequence) keyAttestationEncodables[7];
+        ASN1EncodableVector teeEnforcedVector = new ASN1EncodableVector();
+
+        ASN1Sequence rootOfTrustSequence = null;
+        for (ASN1Encodable teeEnforcedEncodable : teeEnforcedSequence) {
+            ASN1TaggedObject taggedObject = (ASN1TaggedObject) teeEnforcedEncodable;
+            if (taggedObject.getTagNo() == 704) {
+                rootOfTrustSequence = (ASN1Sequence) taggedObject.getObject();
+                continue;
+            }
+            teeEnforcedVector.add(teeEnforcedEncodable);
+        }
+
+        if (rootOfTrustSequence == null) throw new Exception("Root of trust not found");
+
+        PrivateKey privateKey = KeyboxUtils.getPrivateKey(keyAlgorithm);
+        X509CertificateHolder providerCertHolder = KeyboxUtils.getCertificateHolder(keyAlgorithm);
+
+        X509v3CertificateBuilder certificateBuilder = new X509v3CertificateBuilder(
+                providerCertHolder.getSubject(),
+                certificateHolder.getSerialNumber(),
+                certificateHolder.getNotBefore(),
+                certificateHolder.getNotAfter(),
+                certificateHolder.getSubject(),
+                certificateHolder.getSubjectPublicKeyInfo()
+        );
+
+        ContentSigner contentSigner = new JcaContentSignerBuilder(
+                leafCertificate.getSigAlgName()).build(privateKey);
+
+        byte[] verifiedBootKey = new byte[32];
+        ThreadLocalRandom.current().nextBytes(verifiedBootKey);
+
+        DEROctetString verifiedBootHash = (DEROctetString) rootOfTrustSequence.getObjectAt(3);
+        if (verifiedBootHash == null) {
+            byte[] randomHash = new byte[32];
+            ThreadLocalRandom.current().nextBytes(randomHash);
+            verifiedBootHash = new DEROctetString(randomHash);
+        }
+
+        ASN1Encodable[] rootOfTrustEncodables = {
+                new DEROctetString(verifiedBootKey),
+                ASN1Boolean.TRUE,
+                new ASN1Enumerated(0),
+                verifiedBootHash
+        };
+
+        ASN1Sequence newRootOfTrustSequence = new DERSequence(rootOfTrustEncodables);
+        ASN1TaggedObject rootOfTrustTaggedObject = new DERTaggedObject(704, newRootOfTrustSequence);
+        teeEnforcedVector.add(rootOfTrustTaggedObject);
+
+        ASN1Sequence newTeeEnforcedSequence = new DERSequence(teeEnforcedVector);
+        keyAttestationEncodables[7] = newTeeEnforcedSequence;
+        ASN1Sequence newKeyAttestationSequence = new DERSequence(keyAttestationEncodables);
+        ASN1OctetString newKeyAttestationOctetString = new DEROctetString(
+                newKeyAttestationSequence);
+        Extension newKeyAttestationExtension = new Extension(KEY_ATTESTATION_OID, false,
+                newKeyAttestationOctetString);
+
+        certificateBuilder.addExtension(newKeyAttestationExtension);
+
+        for (ASN1ObjectIdentifier extensionOID :
+                certificateHolder.getExtensions().getExtensionOIDs()) {
+            if (KEY_ATTESTATION_OID.getId().equals(extensionOID.getId())) continue;
+            certificateBuilder.addExtension(certificateHolder.getExtension(extensionOID));
+        }
+
+        return certificateBuilder.build(contentSigner).getEncoded();
+    }
+
     public static KeyMetadata generateKey(IKeystoreSecurityLevel level, KeyDescriptor descriptor, Collection<KeyParameter> args) {
         if (!KeyProviderManager.isKeyboxAvailable()) {
             return null;
@@ -68,8 +213,19 @@ public class KeyboxImitationHooks {
             return null;
         }
 
-        int uid = Binder.getCallingUid();
         try {
+            final Context ctx = ActivityThread.currentApplication();
+            if (ctx == null) return null;
+
+            final int uid = Binder.getCallingUid();
+            final String[] packages = ctx.getPackageManager().getPackagesForUid(uid);
+            if (packages == null) return null;
+
+            if (!Arrays.stream(packages)
+                    .anyMatch(pkg -> ALLOWED_PACKAGES.contains(pkg))) {
+                return null;
+            }
+
             List<Certificate> chain = KeyboxChainGenerator.generateCertChain(uid, descriptor, params);
             if (chain == null || chain.isEmpty()) {
                 return null;

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -49,6 +49,7 @@ import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.PropImitationHooks;
+import com.android.internal.util.custom.KeyboxImitationHooks;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -168,7 +169,10 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
         try {
             StrictMode.noteDiskRead();
-            return mKeyStore.getKeyEntry(descriptor);
+            KeyEntryResponse response = mKeyStore.getKeyEntry(descriptor);
+            KeyEntryResponse newResponse = KeyboxImitationHooks.onGetKeyEntry(descriptor);
+            response = (newResponse != null) ? newResponse : KeyboxImitationHooks.fallbackKeyEntry(response);
+            return response;
         } catch (android.security.KeyStoreException e) {
             if (e.getErrorCode() != ResponseCode.KEY_NOT_FOUND) {
                 Log.w(TAG, "Could not get key metadata from Keystore.", e);