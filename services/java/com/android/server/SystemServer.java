ProjectPixelage/android_platform_frameworks_base

PIHooks: Dynamically spoof props for GMS
neobuddy89: Adapted for PixelPropsUtils. Use local props as fallback.

[clarencelol: Adapt changes from CrDroid.]

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: clarencelol <clarencekuiek@proton.me>

services/java/com/android/server/SystemServer.java
@@ -207,6 +207,7 @@ import com.android.server.os.SchedulingPolicyService;
 import com.android.server.pdb.PersistentDataBlockService;
 import com.android.server.people.PeopleService;
 import com.android.server.permission.access.AccessCheckingService;
+import com.android.server.pixelage.AttestationService;
 import com.android.server.pixelage.CustomDeviceConfigService;
 import com.android.server.pm.ApexManager;
 import com.android.server.pm.ApexSystemServiceInfo;
@@ -2712,6 +2713,11 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(BackgroundInstallControlService.class);
                 t.traceEnd();
             }
+
+            // AttestationService
+            t.traceBegin("AttestationService");
+            mSystemServiceManager.startService(AttestationService.class);
+            t.traceEnd();
         }
 
         t.traceBegin("StartMediaProjectionManager");

UOTAN-OS/platform_frameworks_base

Change-Id: I3a7266947e9b7eee9738807448a2f2babaf819e9

services/java/com/android/server/SystemServer.java
@@ -216,6 +216,7 @@ import com.android.server.pdb.PersistentDataBlockService;
 import com.android.server.people.PeopleService;
 import com.android.server.permission.access.AccessCheckingService;
 import com.android.server.pinner.PinnerService;
+import com.android.server.custom.AttestationService;
 import com.android.server.pm.ApexManager;
 import com.android.server.pm.ApexSystemServiceInfo;
 import com.android.server.pm.BackgroundInstallControlService;
@@ -2832,6 +2833,11 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartHealthService");
             mSystemServiceManager.startService(HealthInterfaceService.class);
+            t.traceEnd();
+
+            // AttestationService
+            t.traceBegin("AttestationService");
+            mSystemServiceManager.startService(AttestationService.class);
             t.traceEnd();
         }
 