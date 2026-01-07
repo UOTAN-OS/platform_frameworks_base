96f319ccad34ef4b908a90ac327a40ff24d34ac1
kleidione Freitas <kleidione@gmail.com>
2026-01-07 14:39:09 -0300

    SystemUI: Adapt SSDataProviders to use ViewGroup instead
    Change-Id: Ide3b74b921b2ca07d670baf5db81a688bacf786b
    Signed-off-by: Kleidione Freitas <kleidione@gmail.com>
    Signed-off-by: hridaya <hridaya@pixelos.net>

--- packages/SystemUI/src/com/google/android/systemui/smartspace/DateSmartspaceDataProvider.java
+++ packages/SystemUI/src/com/google/android/systemui/smartspace/DateSmartspaceDataProvider.java
@@ -56,18 +56,18 @@ public final class DateSmartspaceDataProvider implements BcSmartspaceDataPlugin 
     }
 
     @Override
-    public final BcSmartspaceDataPlugin.SmartspaceView getLargeClockView(Context context) {
-        View view = LayoutInflater.from(context).inflate(
-                R.layout.date_plus_extras_large, (ViewGroup) null, false);
+    public final BcSmartspaceDataPlugin.SmartspaceView getLargeClockView(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.date_plus_extras_large, (ViewGroup) null, false);
         view.setId(R.id.date_smartspace_view_large);
         view.addOnAttachStateChangeListener(this.mStateChangeListener);
         return (BcSmartspaceDataPlugin.SmartspaceView) view;
     }
 
     @Override
-    public final BcSmartspaceDataPlugin.SmartspaceView getView(Context context) {
-        View view = LayoutInflater.from(context).inflate(
-                R.layout.date_plus_extras, (ViewGroup) null, false);
+    public final BcSmartspaceDataPlugin.SmartspaceView getView(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.date_plus_extras, (ViewGroup) null, false);
         view.addOnAttachStateChangeListener(this.mStateChangeListener);
         return (BcSmartspaceDataPlugin.SmartspaceView) view;
     }
