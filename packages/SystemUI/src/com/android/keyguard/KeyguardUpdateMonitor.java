f5faf895137d4f4b74099d4c90f9f102b530f9de
Michael Bestas <mkbestas@gmail.com>
2026-06-18 02:52:49 +0300

    Revert "SystemUI: Restore 14 QPR1 FingerprintInteractiveToAuth handling"
    This reverts commit acbd58382c61b7e4579c9c849937971ca82fbe0b.
    
    Change-Id: If6a700d275a989c1542852adb49bc3341c5f21b7

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -3145,14 +3145,9 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
         final boolean shouldListenSecureLockDeviceState = !secureLockDevice()
                 || !mIsSecureLockDeviceEnabled || mSecureLockDeviceListeningForBiometrics;
 
-        final boolean shouldListenFpsState = isUdfps
-                || mFingerprintInteractiveToAuthProvider == null
-                || !mFingerprintInteractiveToAuthProvider.isEnabled(user)
-                || (isDeviceInteractive() && !mGoingToSleep);
-
         boolean shouldListen = shouldListenKeyguardState && shouldListenUserState
                 && shouldListenBouncerState && shouldListenUdfpsState && !mBiometricPromptShowing
-                && shouldListenSecureLockDeviceState && shouldListenFpsState;
+                && shouldListenSecureLockDeviceState;
         logListenerModelData(
                 new KeyguardFingerprintListenModel(
                     System.currentTimeMillis(),
