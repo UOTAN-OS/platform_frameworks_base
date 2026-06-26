f8af6cc773a4ea67f92adf193b0d39c8c91a4eda
uwu-gl <moegluwu0@gmail.com>
2026-06-27 02:47:35 +0800

    SystemUI: Dynamically set scale for StatusBarLyric
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/util/StatusBarLyricLayout.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2026 The uwuAOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.util;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import com.android.systemui.res.R;
+
+public class StatusBarLyricLayout extends LinearLayout {
+    private final int mMaxHeight;
+
+    public StatusBarLyricLayout(Context context) {
+        this(context, null);
+    }
+
+    public StatusBarLyricLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public StatusBarLyricLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public StatusBarLyricLayout(
+            Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        mMaxHeight = getResources().getDimensionPixelSize(R.dimen.lyric_max_text_stack_height);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        if (heightMode != MeasureSpec.UNSPECIFIED && heightSize > 0) {
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(
+                    Math.min(heightSize, mMaxHeight), MeasureSpec.EXACTLY);
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
