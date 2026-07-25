HaloUI-AOSP/android_frameworks_base

Screenrecord: Hide HEVC option when no HW codec is available
Change-Id: I1e5c265980f34a943b2466b1d84d57e009961f8d

packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionContentManager.kt
@@ -21,6 +21,9 @@ import android.app.Activity
 import android.app.PendingIntent
 import android.content.Intent
 import android.hardware.display.DisplayManager
+import android.media.MediaCodecInfo
+import android.media.MediaCodecList
+import android.media.MediaFormat
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -186,6 +189,12 @@ class ScreenRecordPermissionContentManager(
             audioSwitch.isChecked = true
         }
 
+        // Disable HEVC when hardware accelerated codec is not available
+        if (!hasHevcHwEncoder()) {
+            Prefs.putInt(userContextProvider.userContext, PREF_HEVC, 0)
+            containerView.requireViewById<View>(R.id.show_hevc).visibility = View.GONE
+        }
+
         // disable redundant Touch & Hold accessibility action for Switch Access
         options.accessibilityDelegate =
             object : View.AccessibilityDelegate() {
@@ -255,6 +264,24 @@ class ScreenRecordPermissionContentManager(
             )
         controller.startCountdown(if (skipTimeSwitch.isChecked) NO_DELAY else DELAY_MS,
                 INTERVAL_MS, startIntent, stopIntent)
+    }
+
+    private fun hasHevcHwEncoder(): Boolean {
+        val mediaCodecList = MediaCodecList(MediaCodecList.REGULAR_CODECS)
+
+        for (codecInfo in mediaCodecList.getCodecInfos()) {
+            if (!codecInfo.isEncoder() || !codecInfo.isHardwareAccelerated()) {
+                continue
+            }
+
+            for (type in codecInfo.getSupportedTypes()) {
+                if (type.equals(MediaFormat.MIMETYPE_VIDEO_HEVC, ignoreCase = true)) {
+                    return true
+                }
+            }
+        }
+
+        return false
     }
 
     private inner class CaptureTargetResultReceiver :

UOTAN-OS/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionContentManager.kt
@@ -20,6 +20,9 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.hardware.display.DisplayManager
+import android.media.MediaCodecInfo
+import android.media.MediaCodecList
+import android.media.MediaFormat
 import android.media.projection.StopReason
 import android.os.Build
 import android.os.Bundle
@@ -191,6 +194,12 @@ class ScreenRecordPermissionContentManager(
             audioSwitch.isChecked = true
         }
 
+        // Disable HEVC when hardware accelerated codec is not available
+        if (!hasHevcHwEncoder()) {
+            Prefs.putInt(containerView.context, PREF_HEVC, 0)
+            containerView.requireViewById<View>(R.id.show_hevc).visibility = View.GONE
+        }
+
         // disable redundant Touch & Hold accessibility action for Switch Access
         options.accessibilityDelegate =
             object : View.AccessibilityDelegate() {
@@ -253,6 +262,24 @@ class ScreenRecordPermissionContentManager(
             },
             { screenRecordingStartStopRepository.stopRecording(StopReason.STOP_UNKNOWN) },
         )
+    }
+
+    private fun hasHevcHwEncoder(): Boolean {
+        val mediaCodecList = MediaCodecList(MediaCodecList.REGULAR_CODECS)
+
+        for (codecInfo in mediaCodecList.getCodecInfos()) {
+            if (!codecInfo.isEncoder() || !codecInfo.isHardwareAccelerated()) {
+                continue
+            }
+
+            for (type in codecInfo.getSupportedTypes()) {
+                if (type.equals(MediaFormat.MIMETYPE_VIDEO_HEVC, ignoreCase = true)) {
+                    return true
+                }
+            }
+        }
+
+        return false
     }
 
     private inner class CaptureTargetResultReceiver :